798083ba30866d32d3c8a5606c90fcfde5521581
andrewtrz <120880637+andrewtrz@users.noreply.github.com>
2023-03-18 19:31:47 -0400

    Updated heatmap, pub opinion, reqs doc, home page

--- home_page.py
+++ home_page.py
@@ -1,24 +1,48 @@
+import streamlit as st
+import plotly.graph_objects as go
+import pandas as pd
+import plotly.express as px
+import numpy as np
+
 st.set_page_config(layout="wide")
 
 st.sidebar.info(
     """
     - Web App URL: <insert web app details>
-    - GitHub repository: <insert github repo>
+    - GitHub repository: <https://github.com/jordancallahan/DPI852M-Group-1-Final-Project>
     """
 )
 
-st.sidebar.title("Contact")
+st.sidebar.title("Team Members")
 st.sidebar.info(
     """
-   Insert team details/contact
+    Andrew Trzcinski\
+    Jordan Callahan\
+    Sammy Cervantes\
+    May Braverman\
+    Kendrick McDonald
+
     """
 )
 
-st.title("R+D Expenditure, US and Worldwide")
+st.title("R+D Expenditure - US and Worldwide")
+
+st.subheader("Introduction")
+st.markdown(
+    """
+    Welcome to our website, folks! We are a group of six students from the Harvard Kennedy School, taking an Advanced Data Visualization course with Professor Hong Qu. Our project focuses on research and development (R&D) spending, with a view to exploring the trends and patterns of R&D spending both in the United States and worldwide. We will also look at the distribution of R&D spending across the US and its impact on several related topics.
+
+We believe in presenting our research in a way that everyone can understand, so we have utilized a wide variety data visualization techniques (all in one simple webpage) to create visuals that will help convey our insights in a simple and accessible way. Our hope is that this will help policymakers or anyone interested in better understanding the R+D spending landscape can get a good introduction with these visuals.
+
+We're thrilled to have this opportunity to showcase our findings on this platform, and we hope you find our research and analysis interesting and informative. So go ahead, take a look around, and feel free to explore the data sets that we used to make our discoveries. Thanks for stopping by, and we hope you enjoy the website!
+"""
+)
+
+st.info("Click on the left sidebar menu to navigate through each topic.")
 
 st.markdown(
     """
-    We made this multi-page web app to vizually explore science and R&D spending in the US with global comparisons. The project should aim to answer questions such as:
+    We made this multi-page web app to vizually explore science and R&D spending - the project aims to consider questions such as:
 1.       How does science and R&D spending vary across different regions of the US and the world?
 2.       What are the trends in higher education in science and engineering?
 3.       What are the links between science and R&D spending with innovation?
@@ -30,20 +54,7 @@ Much of this project was created using [streamlit](https://streamlit.io) and ope
     """
 )
 
-st.info("Click on the left sidebar menu to navigate through each topic.")
 
-st.subheader("")
-st.markdown(
-    """
-    insert more background text here - the gifs are a joke but we can add more text/video here
-"""
-)
 
-row1_col1, row1_col2 = st.columns(2)
-with row1_col1:
-    st.image("https://github.com/jordancallahan/DPI852M-Group-1-Final-Project/blob/Andrew/data/gifs/chips.gif")
-    st.image("https://github.com/jordancallahan/DPI852M-Group-1-Final-Project/blob/Andrew/data/gifs/science.gif")
 
-with row1_col2:
-    st.image("https://github.com/jordancallahan/DPI852M-Group-1-Final-Project/blob/Andrew/data/gifs/robot.gif")
-    st.image("https://github.com/jordancallahan/DPI852M-Group-1-Final-Project/blob/Andrew/data/gifs/network.gif")
+
--- pages/1_🔥Heatmap.py
+++ pages/1_🔥Heatmap.py
@@ -1,14 +1,11 @@
 import streamlit as st
-import leafmap.foliumap as leafmap
 import pandas as pd
-import json
-from collections import OrderedDict
+import plotly.express as px
 
 st.set_page_config(layout="wide")
 
 st.sidebar.info(
     """
-   
     - Web App URL: <insert home link here>
     - GitHub repository: <insert github link here>
     """
@@ -21,76 +18,37 @@ st.sidebar.info(
     """
 )
 
-st.title("Heatmap of R&D Spending by State")
+# Load data
+data = pd.read_excel("data/tables/R_and_D_by_state.xlsx")
 
-import pandas as pd
+# Replace state names with their two-letter abbreviations
+state_abbr = {'Alabama': 'AL', 'Alaska': 'AK', 'Arizona': 'AZ', 'Arkansas': 'AR', 'California': 'CA', 'Colorado': 'CO', 'Connecticut': 'CT', 'Delaware': 'DE', 'Florida': 'FL', 'Georgia': 'GA', 'Hawaii': 'HI', 'Idaho': 'ID', 'Illinois': 'IL', 'Indiana': 'IN', 'Iowa': 'IA', 'Kansas': 'KS', 'Kentucky': 'KY', 'Louisiana': 'LA', 'Maine': 'ME', 'Maryland': 'MD', 'Massachusetts': 'MA', 'Michigan': 'MI', 'Minnesota': 'MN', 'Mississippi': 'MS', 'Missouri': 'MO', 'Montana': 'MT', 'Nebraska': 'NE', 'Nevada': 'NV', 'New Hampshire': 'NH', 'New Jersey': 'NJ', 'New Mexico': 'NM', 'New York': 'NY', 'North Carolina': 'NC', 'North Dakota': 'ND', 'Ohio': 'OH', 'Oklahoma': 'OK', 'Oregon': 'OR', 'Pennsylvania': 'PA', 'Rhode Island': 'RI', 'South Carolina': 'SC', 'South Dakota': 'SD', 'Tennessee': 'TN', 'Texas': 'TX', 'Utah': 'UT', 'Vermont': 'VT', 'Virginia': 'VA', 'Washington': 'WA', 'West Virginia': 'WV', 'Wisconsin': 'WI', 'Wyoming': 'WY'}
 
-filepath = "https://raw.githubusercontent.com/jordancallahan/DPI852M-Group-1-Final-Project/main/data/tables/R_and_D_by_state.csv"
-df = pd.read_csv(filepath)
-df = df.replace(",", "", regex=True).fillna("0")
+data['State'] = data['State'].map(state_abbr)
 
-with st.expander("See source code"):
-    with st.echo():
-        filepath = df
-        m = leafmap.Map(center=[40, -100], zoom=4, tiles="stamentoner")
+# Create chloroplet map
+st.title("R+D Spending by State Over Time")
+fig = px.choropleth(data, locations="State",
+                    locationmode="USA-states",
+                    scope="usa",
+                    color_continuous_scale="blues",
+                    title="Spend between 2009 - 2021",
+                    animation_frame="Year",
+                    color="Spend",
+                    range_color=[0, 30000000])
 
-        # Load the CSV data and preprocess the data
-        df = pd.read_csv(filepath)
-        df = df.replace("NA", "0")
-        for year in range(2006, 2022):
-            col_name = str(year)
-            df[col_name] = df[col_name].str.replace(",", "").astype(int)
 
-        # Add a slider to select the year
-        min_year = 2006
-        max_year = 2021
-        selected_year = st.sidebar.slider("Select year", min_year, max_year, max_year)
-        selected_column = f"{selected_year}"
+#show map
+st.plotly_chart(fig)
 
-        m.add_heatmap(
-            data=df,
-            latitude="Latitude",
-            longitude="Longitude",
-            value=selected_column,
-            name="Heat map",
-            radius=20,
-        )
-m.to_streamlit(height=700)
-
-st.title("Animated Heatmap of R&D Spending by State")
-
-# Create the animated heatmap
-m_animated = leafmap.Map(center=[40, -100], zoom=4, tiles="stamentoner")
-
-# Prepare the data for the time_slider_choropleth() function
-data = []
-for year in range(min_year, max_year + 1):
-    year_data = df[["State", "Latitude", "Longitude", str(year)]]
-    year_data_dict = year_data.to_dict(orient="records")
-    year_data_json = json.dumps(year_data_dict, ensure_ascii=False)
-    data.append(OrderedDict({"time": str(year), "data": year_data_json}))
-
-# Add the time_slider_choropleth() function to create the animated heatmap
-m_animated.time_slider_choropleth(
-    data=data,
-    latitude="Latitude",
-    longitude="Longitude",
-    weight=str(year),
-    time_interval=1000,
-    auto_play=True,
-    radius=20,
-)
-
-m_animated.to_streamlit(height=700)
 
 
 st.markdown(
-       """
+    """
 From a national perspective, research and development (R&D) spending in the United States has experienced considerable growth between 2006 and 2021. This expansion can be attributed to various factors, including increased government investments, a growing emphasis on technological innovation, and the rise of private sector R&D efforts. The United States has maintained its position as one of the world's top spenders on R&D, with industries such as technology, pharmaceuticals, and aerospace driving much of this investment. Additionally, federal policies, tax incentives, and funding programs have played a crucial role in promoting research in both the public and private sectors. This continuous growth in R&D spending has helped to fuel advancements in technology, healthcare, and other areas, further solidifying the United States' position as a global leader in innovation.
 
 From a state-by-state perspective, there have been notable differences in R&D spending throughout the years. States with strong technology and research sectors, such as California, Massachusetts, and Washington, have consistently been at the forefront of R&D expenditure. These states benefit from the presence of major technology companies, prestigious research institutions, and a highly skilled workforce, which collectively contribute to their high levels of investment. In other states, such as Texas and North Carolina, R&D spending has also seen substantial growth, driven by factors such as the expansion of research universities, targeted economic development initiatives, and the growth of industries like biotechnology and renewable energy.
 
 However, not all states have experienced the same level of growth in R&D spending. Some states have faced economic challenges or have not been able to attract the same level of investment as others. Factors such as geographic location, the availability of skilled labor, and the presence of research-intensive industries can significantly impact a state's ability to increase R&D spending. Overall, the landscape of R&D spending in the United States between 2006 and 2021 has been one of both growth and disparity, with some states outpacing others in their pursuit of research-driven innovation.
-
     """
 )
