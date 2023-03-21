4986f916acf0fac965f9d2348754602a658a286d
Jordan Callahan <32796129+jordancallahan@users.noreply.github.com>
2023-03-21 19:41:54 -0400

    update code formatting

--- home_page.py
+++ home_page.py
@@ -1,8 +1,4 @@
 import streamlit as st
-import plotly.graph_objects as go
-import pandas as pd
-import plotly.express as px
-import numpy as np
 
 st.set_page_config(layout="wide", page_title="R+D Expenditure - US and Worldwide")
 
@@ -30,18 +26,34 @@ st.title("Science, Technology, Research, and Development Indicators - US and Wor
 st.subheader("Introduction")
 st.markdown(
     """
-    Welcome to our website, folks! We are a group of five students from the Harvard Kennedy School, taking an Advanced Data Visualization course with Professor Hong Qu. Our project focuses on indicators related to Science, Technology, Research, and Development, with a view to exploring the trends and patterns both in the United States and worldwide.
+    Welcome to our website, folks! We are a group of five students from the Harvard
+    Kennedy School, taking an Advanced Data Visualization course with Professor Hong
+    Qu. Our project focuses on indicators related to Science, Technology, Research, and
+    Development, with a view to exploring the trends and patterns both in the United
+    States and worldwide.
 
-We believe in presenting our research in a way that everyone can understand, so we have utilized a wide variety data visualization techniques (all in one simple webpage) to create visuals that will help convey our insights in a simple and accessible way. Our hope is that this will help policymakers or anyone interested in better understanding the R+D spending landscape can get a good introduction with these visuals. We have a mix of skillsets on the team so have also worked in a way that allowed for both collaboration and comfort - helping eachother where we we able to and working on ways to harmonize multiple platforms.
+    We believe in presenting our research in a way that everyone can understand, so we
+    have utilized a wide variety data visualization techniques (all in one simple
+    webpage) to create visuals that will help convey our insights in a simple and
+    accessible way. Our hope is that this will help policymakers or anyone interested
+    in better understanding the R+D spending landscape can get a good introduction with
+    these visuals. We have a mix of skillsets on the team so have also worked in a way
+    that allowed for both collaboration and comfort - helping eachother where we we
+    able to and working on ways to harmonize multiple platforms.
 
-We're thrilled to have this opportunity to showcase our findings on this platform, and we hope you find our research and analysis interesting and informative. So go ahead, take a look around, and feel free to explore the data sets that we used to make our discoveries (please explore our GitHub too - that architecture was a fair amount of the project on its own!). Thanks for stopping by, and we hope you enjoy the website!
+    We're thrilled to have this opportunity to showcase our findings on this platform,
+    and we hope you find our research and analysis interesting and informative. So go
+    ahead, take a look around, and feel free to explore the data sets that we used to
+    make our discoveries (please explore our GitHub too - that architecture was a fair
+    amount of the project on its own!). Thanks for stopping by, and we hope you enjoy
+    the website!
 """
 )
 
 
 giphy_url = "https://giphy.com/embed/f9NaK0iIjpY5Vm80a0"
 
-st.components.v1.html(
+st.components.v1.html(  # type: ignore
     f'<div style="display:flex; justify-content:center;"><iframe src="{giphy_url}" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>'
 )
 
--- pages/1_💰US_Spending.py
+++ pages/1_💰US_Spending.py
@@ -6,17 +6,22 @@ st.set_page_config(layout="wide")
 
 st.sidebar.info(
     """
-    - Web App URL: <insert home link here>
-    - GitHub repository: <insert github link here>
+    - Web App [URL](TBU)
+    - GitHub [repository](https://github.com/jordancallahan/DPI852M-Group-1-Final-Project)
     """
 )
 
-st.sidebar.title("Contact")
+st.sidebar.title("Team Members")
 st.sidebar.info(
     """
-   Add team info here
+    * Andrew Trzcinski
+    * Jordan Callahan
+    * Sammy Cervantes
+    * May Braverman
+    * Kendrick McDonald
     """
 )
+
 
 # Load data
 data = pd.read_excel("data/tables/R_and_D_by_state.xlsx")
@@ -114,10 +119,35 @@ st.plotly_chart(fig)
 
 st.markdown(
     """
-From a national perspective, research and development (R&D) spending in the United States has experienced considerable growth between 2006 and 2021. This expansion can be attributed to various factors, including increased government investments, a growing emphasis on technological innovation, and the rise of private sector R&D efforts. The United States has maintained its position as one of the world's top spenders on R&D, with industries such as technology, pharmaceuticals, and aerospace driving much of this investment. Additionally, federal policies, tax incentives, and funding programs have played a crucial role in promoting research in both the public and private sectors. This continuous growth in R&D spending has helped to fuel advancements in technology, healthcare, and other areas, further solidifying the United States' position as a global leader in innovation.
+From a national perspective, research and development (R&D) spending in the United
+States has experienced considerable growth between 2006 and 2021. This expansion can be
+attributed to various factors, including increased government investments, a growing
+emphasis on technological innovation, and the rise of private sector R&D efforts. The
+United States has maintained its position as one of the world's top spenders on R&D,
+with industries such as technology, pharmaceuticals, and aerospace driving much of this
+investment. Additionally, federal policies, tax incentives, and funding programs have
+played a crucial role in promoting research in both the public and private sectors.
+This continuous growth in R&D spending has helped to fuel advancements in technology,
+healthcare, and other areas, further solidifying the United States' position as a
+global leader in innovation.
 
-From a state-by-state perspective, there have been notable differences in R&D spending throughout the years. States with strong technology and research sectors, such as California, Massachusetts, and Washington, have consistently been at the forefront of R&D expenditure. These states benefit from the presence of major technology companies, prestigious research institutions, and a highly skilled workforce, which collectively contribute to their high levels of investment. In other states, such as Texas and North Carolina, R&D spending has also seen substantial growth, driven by factors such as the expansion of research universities, targeted economic development initiatives, and the growth of industries like biotechnology and renewable energy.
+From a state-by-state perspective, there have been notable differences in R&D spending
+throughout the years. States with strong technology and research sectors, such as
+California, Massachusetts, and Washington, have consistently been at the forefront of
+R&D expenditure. These states benefit from the presence of major technology companies,
+prestigious research institutions, and a highly skilled workforce, which collectively
+contribute to their high levels of investment. In other states, such as Texas and North
+Carolina, R&D spending has also seen substantial growth, driven by factors such as the
+expansion of research universities, targeted economic development initiatives, and the
+growth of industries like biotechnology and renewable energy.
 
-However, not all states have experienced the same level of growth in R&D spending. Some states have faced economic challenges or have not been able to attract the same level of investment as others. Factors such as geographic location, the availability of skilled labor, and the presence of research-intensive industries can significantly impact a state's ability to increase R&D spending. Overall, the landscape of R&D spending in the United States between 2006 and 2021 has been one of both growth and disparity, with some states outpacing others in their pursuit of research-driven innovation.
+However, not all states have experienced the same level of growth in R&D spending. Some
+states have faced economic challenges or have not been able to attract the same level
+of investment as others. Factors such as geographic location, the availability of
+skilled labor, and the presence of research-intensive industries can significantly
+impact a state's ability to increase R&D spending. Overall, the landscape of R&D
+spending in the United States between 2006 and 2021 has been one of both growth and
+disparity, with some states outpacing others in their pursuit of research-driven
+innovation.
     """
 )
