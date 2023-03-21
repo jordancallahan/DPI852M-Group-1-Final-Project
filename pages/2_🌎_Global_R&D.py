8b23d59eff90f7d2f55a1ec7454a6a5b3b61060b
Jordan Callahan <32796129+jordancallahan@users.noreply.github.com>
2023-03-21 03:26:39 -0400

    add skeleton for global R&D viz

--- pages/2_🌎_Global_R&D.py
+++ pages/2_🌎_Global_R&D.py
@@ -13,7 +13,6 @@ df = pd.read_csv("data/tables/global_domestic_RD_spend.csv")
 # Drop the INDICATOR, SUBJECT, FREQUENCY, and Flag Codes columns
 df = df.drop(columns=["INDICATOR", "SUBJECT", "FREQUENCY", "Flag Codes"])
 
-
 # Data source: https://data.oecd.org/rd/gross-domestic-spending-on-r-d.htm
 
 # Filter the data by MEASURE: "MLN_USD" and extract the required columns
@@ -30,14 +29,29 @@ fig = px.choropleth(
     projection="natural earth",
     animation_frame="TIME",
     height=600,
+    title="R&D Expenditures by Country (Millions USD)",
 )
 
-# Configure the layout
-fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
+# Filter the data by MEASURE: "PC_GDP" and extract the required columns
+country_data = df[df["MEASURE"] == "PC_GDP"][["LOCATION", "TIME", "Value"]]
+
+# Create the heatmap using Plotly
+fig2 = px.choropleth(
+    country_data,
+    locations="LOCATION",
+    color="Value",
+    hover_name="LOCATION",
+    color_continuous_scale="Viridis",
+    scope="world",
+    projection="natural earth",
+    animation_frame="TIME",
+    height=600,
+    title="R&D Expenditures by Country (Percent of GDP)",
+)
 
 # Streamlit app
-st.title("Global R&D Expenditures by Country")
 st.plotly_chart(fig)
+st.plotly_chart(fig2)
 
 if __name__ == "__main__":
     st.write("Running Streamlit App")
