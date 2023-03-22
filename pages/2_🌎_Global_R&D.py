6d269a2103f09a4a0bfb6e1c2a4bc6345641093e
Jordan Callahan <32796129+jordancallahan@users.noreply.github.com>
2023-03-22 13:58:31 -0400

    update TIME to int on global page

--- pages/2_🌎_Global_R&D.py
+++ pages/2_🌎_Global_R&D.py
@@ -86,8 +86,10 @@ merged_data = world_geojson.merge(df, left_on="name", right_on="name", how="left
 
 merged_data = merged_data.sort_values(by="TIME", ascending=True)
 
-# Convert TIME back to an int
-merged_data["TIME"] = merged_data["TIME"].astype(int)
+# Convert TIME to int, ignoring NaN values
+merged_data["TIME"] = pd.to_numeric(
+    merged_data["TIME"], downcast="integer", errors="coerce"
+)
 
 # Total R&D Expenditures by Country
 # Filter the data by MEASURE: "MLN_USD"
